d619ba20668774ce24765c0b283e453bead809fa
gp789 <info@gemma-partners.com>
2025-09-07 09:37:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,22 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // 👇 Important: for GitHub Pages deployment
-  // When you deploy to https://<user>.github.io/<repo>/,
-  // "base" must be "/<repo-name>/"
-  base: mode === "production" ? "/gemma-partners/" : "/",
+  // Custom domain => site is at "/"
+  base: "/",
 
   server: {
     host: "::",
     port: 8080,
   },
-
   plugins: [
     react(),
     mode === "development" && componentTagger(),
   ].filter(Boolean),
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
